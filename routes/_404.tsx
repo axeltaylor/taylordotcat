eb9d4ddc725c323c0e702ed36e0a670db795ffd5
Axel Taylor <axel@taylor.cat>
2023-12-10 15:50:06 +0100

    Update dependencies and fix 404 page

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -4,20 +4,18 @@ export default function Error404() {
   return (
     <>
       <Head>
-        <title>404 - Page not found</title>
+        <title>404 Error | taylordotcat</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
+      <div class="px-4 py-8 mx-auto">
         <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
+          <h1 class="text-4xl font-bold text-center">
+            404: Nop it's not here
+          </h1>
+          <img src="/images/hax-space-not-found.jpg" alt="404" class="mt-3" />
+          <p class="mt-3">
+            Congratulations on your discovery! While you won't find what you
+            were looking for, you've unwittingly entered the exclusive club of
+            unintentional space explorers.
           </p>
           <a href="/" class="underline">Go back home</a>
         </div>
